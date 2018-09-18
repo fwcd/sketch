e932a73cb2146cc6200b91331ad6401f8fe0dc31
fwcd <fwcdmail@gmail.com>
2018-09-18 18:51:59 +0200

    Extracted constants in PolymorphicSerializer

--- src/main/java/com/fwcd/sketch/model/utils/PolymorphicSerializer.java
+++ src/main/java/com/fwcd/sketch/model/utils/PolymorphicSerializer.java
@@ -11,12 +11,15 @@ import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 
 public class PolymorphicSerializer<T> implements JsonSerializer<T>, JsonDeserializer<T> {
+	private static final String INSTANCE_PROPERTY = "INSTANCE";
+	private static final String CLASS_PROPERTY = "CLASSNAME";
+	
 	@Override
 	public JsonElement serialize(T src, Type typeOfSrc, JsonSerializationContext context) {
 		JsonObject serialized = new JsonObject();
 		
-		serialized.add("INSTANCE", context.serialize(src));
-		serialized.addProperty("CLASSNAME", src.getClass().getName());
+		serialized.add(INSTANCE_PROPERTY, context.serialize(src));
+		serialized.addProperty(CLASS_PROPERTY, src.getClass().getName());
 		
 		return serialized;
 	}
@@ -25,8 +28,8 @@ public class PolymorphicSerializer<T> implements JsonSerializer<T>, JsonDeserial
 	public T deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
 		JsonObject serialized = json.getAsJsonObject();
 		
-		String className = serialized.get("CLASSNAME").getAsString();
-		JsonElement instance = serialized.get("INSTANCE");
+		String className = serialized.get(CLASS_PROPERTY).getAsString();
+		JsonElement instance = serialized.get(INSTANCE_PROPERTY);
 		
 		try {
 			return context.deserialize(instance, Class.forName(className));
